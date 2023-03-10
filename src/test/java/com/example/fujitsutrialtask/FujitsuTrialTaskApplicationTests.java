c3437ca61886d47736a2f10c565fa197f23f83db
alpari01 <alan.parik456@gmail.com>
2023-03-10 12:52:17 +0200

    Initial commit

---
+++ src/test/java/com/example/fujitsutrialtask/FujitsuTrialTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fujitsutrialtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FujitsuTrialTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
